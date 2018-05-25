f4495bec90eefc5f1fd3e32963629415122588ca
mgumbar <>
2018-05-25 09:06:17 +0200

    logs component

--- mongoLogUi/src/app/app.module.ts
+++ mongoLogUi/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogsComponent } from './logs/logs.component';
+import { LogListComponent } from './logs/log-list/log-list.component';
+import { LogDetailsComponent } from './logs/log-list/log-details/log-details.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogsComponent,
+    LogListComponent,
+    LogDetailsComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ mongoLogUi/src/app/logs/log-list/log-details/log-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-details',
+  templateUrl: './log-details.component.html',
+  styleUrls: ['./log-details.component.css']
+})
+export class LogDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
